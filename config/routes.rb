5505db7cfc5a252ba6d3a16943b66f6d4d7815c0
christian <christianbiringanine22@gmail.com>
2022-09-28 13:11:30 +0200

    Add the endpoint API and render the static view page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'root/index'
+  # get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    namespace :v1 do
+      resources :messages, only: [:index]
+    end
+  end
   # Defines the root path route ("/")
-  root "root#index"
+  root "static#index"
 end
